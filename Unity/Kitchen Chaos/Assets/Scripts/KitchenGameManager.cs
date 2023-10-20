fed877192abd4d5109a76d67424598519ae2cf7d
ReminaGH <Christoferpworkmail@gmail.com>
2023-10-20 12:03:52 +0200

    handled late joins
    3:28:04

--- Unity/Kitchen Chaos/Assets/Scripts/KitchenGameManager.cs
+++ Unity/Kitchen Chaos/Assets/Scripts/KitchenGameManager.cs
@@ -157,6 +157,10 @@ public class KitchenGameManager : NetworkBehaviour {
         return state.Value == State.GameOver;
     }
 
+    public bool IsWaitingToStart() {
+        return state.Value == State.WaitingToStart;
+    }
+
     public bool IsLocalPlayerReadyer() {
         return isLocalPlayerReady;
     }
