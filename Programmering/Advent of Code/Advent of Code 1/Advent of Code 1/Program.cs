a2d1f2bcc4bac0421d9b470bba30f39a4ea6039c
ReminaGH <108189114+ReminaGH@users.noreply.github.com>
2023-12-02 22:55:16 +0100

    Advent of code

---
+++ Programmering/Advent of Code/Advent of Code 1/Advent of Code 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Advent_Of_Code_1 {
+    static void Main() {
+        const string INPUT_FILE = @"D:\Git\CpStuff\Programmering\Advent of Code\Advent of Code 1\AoC_Input.txt";
+
+        var counter = 0;
+
+        var lines = File.ReadAllLines(INPUT_FILE);
+        foreach (var line in lines) {
+            var cleanLine = line
+                .Replace("one", "o1e")
+                .Replace("two", "t2o")
+                .Replace("three", "t3e")
+                .Replace("four", "f4r")
+                .Replace("five", "f5e")
+                .Replace("six", "s6x")
+                .Replace("seven", "s7n")
+                .Replace("eight", "e8t")
+                .Replace("nine", "n9e");
+
+            // Get the first number from the line
+            var firstNumber = cleanLine.First(Char.IsDigit);
+
+            // Get the Second Number
+            var lastNumber = cleanLine.Last(Char.IsDigit);
+
+            // Concat the first number with the second number
+            var combinedNumber = firstNumber.ToString() + lastNumber.ToString();
+
+            // Convert the combined numbers to an int and add it to the counter
+            counter += int.Parse(combinedNumber);
+        }
+
+        Console.WriteLine(counter);
+    }
+}
---
+++ Programmering/Advent of Code/Advent of Code 2/Advent of Code 2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Advent_of_Code_2 {
+    internal class Program {
+            record Game(int Num, IReadOnlyList<Set> Sets);
+
+            record Set(int Red, int Green, int Blue);
+
+            private static void Main(string[] args) {
+                var inputLines = File.ReadAllLines(@"D:\Git\CpStuff\Programmering\Advent of Code\Advent of Code 2\AoC_Input.txt");
+                var games = inputLines
+                    .Select(l => {
+                        var gameAndSets = l.Split(":");
+                        return new Game(
+                            Num: int.Parse(gameAndSets[0].Split(" ")[1]),
+                            Sets: gameAndSets[1].Split(";")
+                                .Select(subset => {
+                                    var rawSet = subset.Split(",")
+                                        .Select(v => {
+                                            var colorAndValue = v.Split(" ");
+                                            return (Color: colorAndValue[2], Value: int.Parse(colorAndValue[1]));
+                                        }).ToList();
+
+                                    return new Set(
+                                        Red: rawSet.SingleOrDefault(cv => cv.Color is "red").Value,
+                                        Green: rawSet.SingleOrDefault(cv => cv.Color is "green").Value,
+                                        Blue: rawSet.SingleOrDefault(cv => cv.Color is "blue").Value);
+                                }).ToList());
+                    }).ToList();
+
+                var answer1 = games
+                    .Where(g => g.Sets.All(gs => gs is { Red: <= 12, Green: <= 13, Blue: <= 14 }))
+                    .Sum(r => r.Num);
+                Console.WriteLine($"Answer 1: {answer1}");
+
+                var answer2 = games
+                    .Select(g => g.Sets.Max(r => r.Red) * g.Sets.Max(r => r.Green) * g.Sets.Max(r => r.Blue))
+                    .Sum();
+                Console.WriteLine($"Answer 2: {answer2}");
+            }
+        }
+    }
