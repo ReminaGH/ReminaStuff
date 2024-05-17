d45a4c39ad85e7921429b7780765f57fe1fbb3d9
ReminaGH <Christoferpworkmail@gmail.com>
2024-05-17 11:26:37 +0200

    Score stuff

--- Unity/Axevalla-Arkad/Assets/Scripts/UI/ScoreUI.cs
+++ Unity/Axevalla-Arkad/Assets/Scripts/UI/ScoreUI.cs
@@ -2,26 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using System.IO;
-using System.Linq;
 
 public class ScoreUI : MonoBehaviour
 {
+    [SerializeField] private ScoreUI scoreUI;
     [SerializeField] private TextMeshProUGUI scoreUIText;
-    [SerializeField] private BaseCabinet baseCabinet;
 
-    string baseCabinetStringContent;
+    int testScore;
 
-
-    //Returns the file contents of the .txt file using the specicifc file locations provided by the BaseCabinet
-    private void Update() {
-
-
-        baseCabinetStringContent = baseCabinet.GetCurrentScoreLogFile();
-  
-        scoreUIText.text = baseCabinetStringContent;
+    public void Update() {
         
-        
-
+        scoreUIText.text = BaseCabinet.Score.GetCurrentScoreLogFile();
     }
 }
